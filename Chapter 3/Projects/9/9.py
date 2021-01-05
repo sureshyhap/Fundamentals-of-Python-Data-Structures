fb3fb39fd6feb3e2e532e418435afcb47f2a10d7
Suresh Yhap <sureshyhap@gmail.com>
2021-01-05 14:16:15 -0500

    Finished Chapter 3

--- Chapter 3/Projects/9/9.py
+++ Chapter 3/Projects/9/9.py
@@ -1,29 +1,55 @@
-lyst = [6, 1, 5, 2, 8, 5, 1, 1, 2, 1, 5, 9, 8]
+import random, time
 
-def quicksort(lyst, size):
-    quick_sort(lyst, 0, size - 1)
-
-def quick_sort(lyst, left, right):
-    if left >= right:
-        return
-    mid = (left + right) // 2
-    pivot = lyst[mid]
-    lyst[mid], lyst[right] = lyst[right], lyst[mid]
-    i = left
-    j = right - 1
-    while i < j:
-        while lyst[i] < pivot:
-            i += 1
-        while (lyst[j] > pivot) and (j > i):
-            j -= 1
-        if i < j:
-            lyst[i], lyst[j] = lyst[j], lyst[i]
-    lyst[i], lyst[right] = lyst[right], lyst[i]
-    quick_sort(lyst, left, mid - 1)
-    quick_sort(lyst, mid + 1, right)
+def make_random_list(size):
+    lyst = []
+    for i in range(size):
+        while True:
+            num = random.randint(1, size)
+            if not num in lyst:
+                lyst.append(num)
+                break
+    return lyst
     
 
-quicksort(lyst, len(lyst))
-print(lyst)
+def insertion_sort(lyst):
+    size = len(lyst)
+    i = 1
+    while i < size:
+        elem = lyst[i]
+        j = i - 1
+        while elem < lyst[j] and j >= 0:
+            lyst[j + 1] = lyst[j]
+            j -= 1
+        lyst[j + 1] = elem
+        i += 1
+        
 
-########################### ?????????? ###################
+def quick_sort(lyst, left, right):
+    if (right - left) < 50:
+        insertion_sort(lyst)
+    elif left < right:
+        mid = (left + right) // 2
+        lyst[mid], lyst[right] = lyst[right], lyst[mid]
+        pivot = lyst[right]
+        i = left
+        j = right
+        while i < j:
+            while lyst[i] < pivot:
+                i += 1
+            while (lyst[j] > pivot) and (j > i):
+                j -= 1
+            if i < j:
+                lyst[i], lyst[j] = lyst[j], lyst[i]
+                i += 1
+                j -= 1
+        lyst[i], lyst[right] = lyst[right], lyst[i]
+        quick_sort(lyst, left, mid - 1)
+        quick_sort(lyst, mid + 1, right)
+
+        
+lyst = make_random_list(2000)
+t1 = time.time()
+quick_sort(lyst, 0, len(lyst) - 1)
+t2 = time.time()
+time_taken = t2 - t1
+print(time_taken)
